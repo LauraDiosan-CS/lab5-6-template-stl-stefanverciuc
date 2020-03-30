2db6b1efc78e56d1aa52909c39ddb7bf1f2552ef
verciucstefan <verciucs.sv@gmail.com>
2020-03-30 19:54:27 +0300

    no message

--- lab6/lab6/Test.cpp
+++ lab6/lab6/Test.cpp
@@ -14,7 +14,17 @@ void test()
 	char* c;
 	c = p.getTitlu();
 	assert(c[0]=='v');
+	assert(c[1] == 'i');
+	assert(c[2] == 's');
+	c = p.getAutor();
+	assert(c[0] == 'd');
+	assert(c[1] == 'a');
+	assert(c[2] == 'n');
+
+
+
 	r.addElem(p);
+
 	
 
 }
