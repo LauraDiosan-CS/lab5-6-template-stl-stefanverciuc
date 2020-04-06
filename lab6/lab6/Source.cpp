309499479882c42fd386f2533285ce0a4369ca1e
verciucstefan <verciucs.sv@gmail.com>
2020-04-06 20:05:19 +0300

    no message

--- lab6/lab6/Source.cpp
+++ lab6/lab6/Source.cpp
@@ -1,74 +1,42 @@
-#include "Repo.h"
+
 #include "Test.h"
 #include <iostream>
+#include "UI.h"
+
+#include <iostream>
+//#include <fstream>
 
 using namespace std;
 
+void initService(Service& s) {
+	RepoTemplate<Carte> r;
+	//r.load();
+	r.loadFromFile("Carti.txt");
+	
+	s.setRepo(r);
+}
+
 int main() {
+	cout << "start..." << endl;
+	/******TEST REPO FILE****/
+	/*TestRepositoryFile test;
+	test.testAddElem();
+	test.testDelElem();
+	test.testElemAtPos();
+	test.testFindElem();
+	test.testGetAll();
+	test.testSize();
+	test.testUpdateElem();*/
 
+	/******** TEST SERVICE ******/
+/*	TestService ts;
+	ts.runTests();
+	*/
 	test();
-
-	Repo r;
-	int i = 1;
-	while (i)
-	{
-		cout << "Scrie 1 pentru adaugare,2 afisare, 3 modificare, 4 stergere"<<'\n';
-		cin >> i;
-		if (i == 1) {
-			char titlu[100],autor[100];
-
-			bool ok;
-			cout << "Titlul:";
-			cin >> titlu;
-			cout << "Autor:";
-			cin >> autor;
-			cout<<"Statut:";
-			cin >> ok;
-			Carte p(titlu, autor, ok);
-			r.addElem(p);
-		}
-		if (i == 2)
-		{
-			deque<Carte> carti = r.getAll();
-			for (Carte s : carti)
-				cout << s;
-			cout << '\n';
-		}
-		if (i == 3)
-		{
-			char titlu[100], autor[100];
-
-			bool ok;
-			cout << "Titlul de modificat:";
-			cin >> titlu;
-			cout << "Autor de modificat:";
-			cin >> autor;
-			cout << "Statut de modificat:";
-			cin >> ok;
-			Carte p(titlu, autor, ok);
-			cout << "Titlul:";
-			cin >> titlu;
-			cout << "Autor:";
-			cin >> autor;
-			cout << "Statut:";
-			cin >> ok;
-
-			r.updateElem(p, titlu, autor, ok);
-		}
-		if (i == 4)
-		{
-			char titlu[100], autor[100];
-
-			bool ok;
-			cout << "Titlul de modificat:";
-			cin >> titlu;
-			cout << "Autor de modificat:";
-			cin >> autor;
-			cout << "Statut de modificat:";
-			cin >> ok;
-			Carte p(titlu, autor, ok);
-			r.delElem(p);
-		}
-	}
+	testservice();
+	Service serv;
+	initService(serv);
+	showUI(serv);
+	//cout << "succes";
 	return 0;
 }
--- lab6/lab6/Test.cpp
+++ lab6/lab6/Test.cpp
@@ -1,15 +1,19 @@
-#include "Repo.h"
+#include "Repo_Template.h"
+#include "Service.h"
 #include <assert.h>
 #include <iostream>
+
 
 using namespace std;
 
 void test()
 {
-	Repo r;
+	RepoTemplate<Carte> r;
 	char t[]="vis", a[]="dan";
+	char t1[] = "aur", a1[] = "dan";
 	bool ok = 0;
 	Carte p(t, a, ok);
+	Carte q(t1, a1, ok);
 	assert(p.getStatut() == ok);
 	char* c;
 	c = p.getTitlu();
@@ -22,9 +26,53 @@ void test()
 	assert(c[2] == 'n');
 
 
+	//cout << "Run test";
+	r.add(p);
+	deque<Carte> all;
+	
+	
+	int n = r.size();
+	assert(n == 1);
 
-	r.addElem(p);
+	r.update(p, q);
+	//all = r.getAll();
+	deque<Carte>::iterator it;
+	it = all.begin();
+	//assert((*it) == q);
+	//assert(all.at(0) == q);
+
+	r.remove(q);
+	assert(r.size() == 0);
+
+
+}
+
+void testservice()
+{
+	RepoTemplate <Carte> r;
+	char t[] = "vis", a[] = "dan";
+	char t1[] = "aur", a1[] = "dan";
+	bool ok = 0;
+	Service s(r);
+	Carte c(t, a, ok);
+	s.addCarte(c);
+	deque<Carte> all=s.getAll();
+	assert(all.size() == 1);
+	//assert(r.size() == 1);
+	assert(s.findOne(c) >= 0);
+	s.delCarte(c);
+	all = s.getAll();
+	assert(r.size() == 0);
+	cout << "Run test service" << '\n';
+
+	s.addCarte(c);
+	deque <Carte> rez;
+	rez=s.filterByAutor(a);
+	assert(rez.size() == 1);
+
 
 	
 
+
 }
+
